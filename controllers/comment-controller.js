230af89326bdbb424f69e85640d9545d961cdf48
Boba Fett <thomascupchurch@gmail.com>
2021-10-04 13:32:24 -0500

    adds error handling to addComment

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -4,9 +4,22 @@ const commentController = {
   // add comment to pizza
   addComment({ params, body }, res) {
     console.log(body);
-    Comment.create(body).then(({ _id }) => {
-      console.log(_id);
-    });
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
   },
 
   // remove comment
