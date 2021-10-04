5b6e962eac7ea5c3d6bbf044cc51d5d5c255d0f4
Boba Fett <thomascupchurch@gmail.com>
2021-10-04 14:07:51 -0500

    adds sorting feature to get all pizzas in descending order

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
