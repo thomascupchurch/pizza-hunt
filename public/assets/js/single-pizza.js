8f47143fb6474a68eed61ddb66f143aa8f54b755
Boba Fett <thomascupchurch@gmail.com>
2021-10-04 15:57:20 -0500

    adds catch method to return to previous page

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -26,7 +26,12 @@ function getPizza() {
 
       return response.json();
     })
-    .then(printPizza);
+    .then(printPizza)
+    .catch((err) => {
+      console.log(err);
+      alert("Cannot find a pizza with this id. Taking you back.");
+      window.history.back();
+    });
 }
 
 function printPizza(pizzaData) {
