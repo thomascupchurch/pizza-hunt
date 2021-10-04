397b399b0a7cca9e73bfab6ba871e8ec4fa179bf
Boba Fett <thomascupchurch@gmail.com>
2021-10-04 13:39:19 -0500

    adds removeComment method

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -23,6 +23,26 @@ const commentController = {
   },
 
   // remove comment
-  removeComment() {},
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then((deletedComment) => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: "No comment with this id." });
+        }
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id." });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
 module.exports = commentController;
