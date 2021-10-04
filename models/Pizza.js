9a82adecb9ce8119f8ae408487d70fedf25831d6
Boba Fett <thomascupchurch@gmail.com>
2021-10-04 12:52:45 -0500

    add comments array field to Pizza.js

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+  pizzaName: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  size: {
+    type: String,
+    default: "Large",
+  },
+  toppings: [],
+  comments: [
+    {
+      type: String,
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+  ],
 });
 
 // create the Pizza model using PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
