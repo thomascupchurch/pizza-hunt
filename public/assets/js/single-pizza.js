fa8c1168094c5622788341ff6eaef1042ad35eab
Boba Fett <thomascupchurch@gmail.com>
2021-10-04 16:03:20 -0500

    complete code for handleNewFormSubmit function

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const $backBtn = document.querySelector("#back-btn");
 const $pizzaName = document.querySelector("#pizza-name");
 const $createdBy = document.querySelector("#created-by");
@@ -126,6 +128,28 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+
+  fetch(`/api/comments/${pizzaId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      response.json();
+    })
+    .then((commentResponse) => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function handleNewReplySubmit(event) {
@@ -153,3 +177,5 @@ $backBtn.addEventListener("click", function () {
 
 $newCommentForm.addEventListener("submit", handleNewCommentSubmit);
 $commentSection.addEventListener("submit", handleNewReplySubmit);
+
+getPizza();
