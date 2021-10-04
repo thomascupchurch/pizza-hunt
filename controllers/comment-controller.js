0886b2303c0f1ccf8cfccb86ceacfb92fd5a0018
Boba Fett <thomascupchurch@gmail.com>
2021-10-04 13:27:34 -0500

    adds addComment method to commentController

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,7 +2,12 @@ const { Comment, Pizza } = require("../models");
 
 const commentController = {
   // add comment to pizza
-  addComment() {},
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body).then(({ _id }) => {
+      console.log(_id);
+    });
+  },
 
   // remove comment
   removeComment() {},
